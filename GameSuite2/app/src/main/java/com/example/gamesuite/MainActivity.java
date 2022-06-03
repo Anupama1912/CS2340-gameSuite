eb25d0a036f486218aab66e1819edd4f6723e4b8
menirmala1 <menirmala1@gmail.com>
2022-06-03 19:14:21 -0400

    togglebotton - enter button - background

--- GameSuite2/app/src/main/java/com/example/gamesuite/MainActivity.java
+++ GameSuite2/app/src/main/java/com/example/gamesuite/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.gamesuite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FrameLayout layout1 = (FrameLayout) findViewById(R.id.layout);
 
         //Wire up exit button for application
         ImageButton btn = (ImageButton) findViewById(R.id.Exit);
@@ -22,6 +27,30 @@ public class MainActivity extends AppCompatActivity {
             Log.i("My app", "This is for testing purposes that Exit Button Works!");
             Toast.makeText(getApplicationContext(), "Exit Application", Toast.LENGTH_SHORT)
                     .show();
+        });
+
+        ToggleButton toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton);
+
+        toggleButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (toggleButton1.isChecked()) {
+                    layout1.setBackgroundResource(R.drawable.night);
+                } else {
+                    layout1.setBackgroundResource(R.drawable.day);
+                }
+            }
+        });
+
+        Button enter = (Button) findViewById(R.id.button);
+        enter.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ GameSuite2/app/src/main/java/com/example/gamesuite/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.gamesuite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ToggleButton;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        FrameLayout layout2 = (FrameLayout) findViewById(R.id.layout);
+
+        ToggleButton toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton);
+
+        toggleButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (toggleButton1.isChecked()) {
+                    layout2.setBackgroundResource(R.drawable.nightplaceholder);
+                } else {
+                    layout2.setBackgroundResource(R.drawable.dayplaceholder);
+                }
+            }
+        });
+    }
+}
