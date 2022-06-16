a9e4c13b81bc9dfcf48c4878215152c29374bdfd
Nirmala <menirmala1@gmail.com>
2022-06-16 19:58:40 -0400

    changed the method to create the wordle grid

--- GameSuite2/app/src/main/java/com/example/gamesuite/WordleActivity.java
+++ GameSuite2/app/src/main/java/com/example/gamesuite/WordleActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
@@ -13,10 +14,13 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class WordleActivity extends AppCompatActivity {
+    public static TextView[][] wordleBoxArray = new TextView[6][5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,7 @@ public class WordleActivity extends AppCompatActivity {
         InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
         keyboard.setInputConnection(ic);
 
-
+        initialiazeArray();
         ImageButton btn = findViewById(R.id.BackGame);
         btn.setOnClickListener(v -> {
             Log.i("My app", "This is for testing purposes that Back Button Works!");
@@ -47,6 +51,21 @@ public class WordleActivity extends AppCompatActivity {
             Log.i("My app", "This is for testing purposes that Info Button works!");
             showInstructions();
         });
+    }
+
+     void initialiazeArray() {
+        GridLayout board = findViewById(R.id.wordleBoard);
+        for (int row = 0; row < 6; row++) {
+            for (int column = 0; column < 5; column++){
+                TextView box = new TextView(this);
+                box.setId(row * 10 + column);
+                box.setBackgroundResource(R.drawable.wordleboxes);
+                box.setHeight(( int) (40 * Resources.getSystem().getDisplayMetrics().density));
+                box.setWidth(( int) (35 * Resources.getSystem().getDisplayMetrics().density));
+                board.addView(box);
+                wordleBoxArray[row][column] = box;
+            }
+        }
     }
 
     void showInstructions() {
