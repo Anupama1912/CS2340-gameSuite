a178a36d0fe0365ef338ccfca79a6bc9be2e1446
Nirmala <menirmala1@gmail.com>
2022-06-26 19:50:05 -0400

    login logic

--- GameSuite2/app/src/main/java/com/example/gamesuite/loginActivity.java
+++ GameSuite2/app/src/main/java/com/example/gamesuite/loginActivity.java
@@ -3,12 +3,44 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class loginActivity extends AppCompatActivity {
+  public class loginActivity extends AppCompatActivity {
 
+    EditText username, password;
+    Button login;
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        DB = new DBHelper(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass)) {
+                    Toast.makeText(loginActivity.this, "All fields REQUIRED", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if (checkuserpass == true) {
+                        Toast.makeText(loginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(loginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
