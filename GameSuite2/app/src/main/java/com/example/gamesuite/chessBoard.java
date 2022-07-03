c4165ad0d114a5eeb6dff39aad7ea11b2595441b
Nirmala <menirmala1@gmail.com>
2022-07-03 18:46:03 -0400

    pieces on board

--- GameSuite2/app/src/main/java/com/example/gamesuite/chessBoard.java
+++ GameSuite2/app/src/main/java/com/example/gamesuite/chessBoard.java
@@ -1,18 +1,25 @@
 package com.example.gamesuite;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import java.util.Map;
+
 public class chessBoard extends View {
     float startleft;
     float starttop;
     float squarelength = 130f;
+    Map<Integer, Bitmap> pieces;
     public chessBoard(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
@@ -20,9 +27,17 @@ public class chessBoard extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        drawBoard(canvas);
+        drawBoard(canvas);
+        pieces.put(R.drawable.blackbishop, BitmapFactory.decodeResource(getResources(), R.drawable.blackbishop));
+        pieces.put(R.drawable.whitebishop, BitmapFactory.decodeResource(getResources(), R.drawable.whitebishop));
+        pieces.put(R.drawable.blackknight, BitmapFactory.decodeResource(getResources(), R.drawable.blackknight));
+        pieces.put(R.drawable.whitebishop, BitmapFactory.decodeResource(getResources(), R.drawable.whitebishop));
+
+    }
+
+    protected void drawBoard(Canvas canvas) {
         Paint paint = new Paint();
-       paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
         startleft = getWidth()/2f - 130f *4;
         starttop = getHeight()/2f - 130f * 4;
         for (int column = 0; column < 8; column++ ) {
@@ -40,9 +55,16 @@ public class chessBoard extends View {
                         paint.setColor(Color.DKGRAY);
                     }
                 }
-                canvas.drawRect(startleft + 130f*column, starttop + 130f * row, startleft + 130f + 130f * column, starttop + 130f + 130f * row, paint);
+                canvas.drawRect(startleft + squarelength*column, starttop + squarelength * row, startleft + squarelength + squarelength * column, starttop + squarelength + squarelength * row, paint);
             }
         }
+    }
+
+    public void drawPiece(Canvas canvas, int column, int row, int pieceId) {
+        startleft = getWidth()/2f - 130f *4;
+        starttop = getHeight()/2f - 130f * 4;
+        Bitmap piece = pieces.get(pieceId);
+        canvas.drawBitmap(piece, null, RectF(startleft + (7 - column)*squarelength, ));
     }
 
 //    private void drawBoard(Canvas canvas) {
