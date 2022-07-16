dfc9b94c049ed1194a73ae27ff58b61669d8d1fa
Anupama1912 <anair325@gatech.edu>
2022-07-16 15:38:45 -0400

    added swipe functionality for princess movement

--- GameSuite2/app/src/main/java/com/example/gamesuite/PrincessChar.java
+++ GameSuite2/app/src/main/java/com/example/gamesuite/PrincessChar.java
@@ -1,15 +1,83 @@
 package com.example.gamesuite;
 
 import android.graphics.Canvas;
+import android.util.Log;
+import android.view.MotionEvent;
 
 public class PrincessChar extends Character{
     boolean supercharged = false;
     int lives = 3;
     int points = 0;
+    String direction = new String("");
+    int downX, downY, upX, upY = 0;
+    final int THRESHOLD = 100;
+    boolean isDone = false;
 
-    @Override
     public void move() {
+        int diffX = upX - downX;
+        int diffY = upY - downY;
+        Log.i("DIFFX", String.valueOf(diffX));
+        Log.i("DIFFY", String.valueOf(diffY));
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (Math.abs(diffX) > THRESHOLD) {
+                if (diffX > 0) {
+                    direction = "RIGHT";
+                } else {
+                    direction = "LEFT";
+                }
+            }
+        } else if (Math.abs(diffY) > THRESHOLD) {
+            if (diffY > 0) {
+                direction = "DOWN";
+            } else {
+                direction = "UP";
+            }
+        }
+        Log.i("DIR", direction);
+        downX = downY = upX = upY = 0;
+        /*
+        while (!isDone) {
+            switch (direction) {
+                case "RIGHT":
+                    // continue moving right
+                    xPos++;
+                    Log.i("xPos", String.valueOf(xPos));
+                    break;
+                case "LEFT":
+                    // continue moving left
+                    xPos--;
+                    break;
+                case "DOWN":
+                    // continue moving down
+                    yPos++;
+                    break;
+                case "UP":
+                    // continue moving up
+                    yPos--;
+                    break;
+            }
+        }
+         */
+    }
 
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = (int) event.getX();
+                downY = (int) event.getY();
+                Log.i("TAG", "touched down " + String.valueOf(downX) + " " + String.valueOf(downY));
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i("TAG", "moving:");
+                break;
+            case MotionEvent.ACTION_UP:
+                upX = (int) event.getX();
+                upY = (int) event.getY();
+                Log.i("TAG", "touched up");
+                move();
+                break;
+        }
+        return true; // ???
     }
 
     @Override
