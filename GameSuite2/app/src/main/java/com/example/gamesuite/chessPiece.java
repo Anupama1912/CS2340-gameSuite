aa2a8baadf673a2b6503a407fb3eace67c17b4c6
IT <ebadallah.seibou@gmail.com>
2022-07-21 16:57:58 -0400

    Changes to abstract class

--- GameSuite2/app/src/main/java/com/example/gamesuite/chessPiece.java
+++ GameSuite2/app/src/main/java/com/example/gamesuite/chessPiece.java
@@ -1,5 +1,9 @@
 package com.example.gamesuite;
 
+import android.util.Pair;
+
+import java.util.List;
+
 public abstract class chessPiece {
     int column;
     int row;
@@ -14,4 +18,26 @@ public abstract class chessPiece {
         this.rank = rank;
         this.id = id;
     }
+
+    /**
+     *  Returns possible moves of this piece
+     * @return A list of pairs representing cells the piece can move to.
+     */
+    abstract List<Pair<Integer, Integer>> getLegalMovements();
+
+    /**
+     * Checks move legality
+     * @param column the column of the cell we're attempting to move to
+     * @param row the row of the cell we're attempting to move to
+     * @return whether the move is legal or not
+     */
+    abstract boolean validateMove(int column, int row);
+
+    /**
+     *
+     * @param column the column of the cell to move to
+     * @param row the row of the cell to move to
+     * @return whether the move was successful
+     */
+    abstract boolean move(int column, int row);
 }
