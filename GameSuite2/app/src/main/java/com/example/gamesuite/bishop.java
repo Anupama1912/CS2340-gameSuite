4476576dd18ce5aa4d9a5c9c852055edb58685db
Abdullah Ahmed <abdullahssjgod@gmail.com>
2022-07-23 15:16:48 -0400

    bishop functionality added

--- GameSuite2/app/src/main/java/com/example/gamesuite/bishop.java
+++ GameSuite2/app/src/main/java/com/example/gamesuite/bishop.java
@@ -6,9 +6,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class bishop extends chessPiece{
+    boolean inCheck;
+    chessColor oppColor;
+    boolean oppInCheck;
+    int oppColumn;
+    int oppRow;
 
     public bishop(int column, int row, chessColor color) {
         super(column, row, color, chessRank.BISHOP, (color == chessColor.BLACK)? R.drawable.blackbishop: R.drawable.whitebishop);
+        if(color == chessColor.BLACK) {
+            inCheck = chessActivity.blackInCheck;
+            oppInCheck = chessActivity.whiteInCheck;
+            oppColor = chessColor.WHITE;
+            oppColumn = chessActivity.whiteColumn;
+            oppRow = chessActivity.whiteRow;
+        } else {
+            inCheck = chessActivity.whiteInCheck;
+            oppInCheck = chessActivity.blackInCheck;
+            oppColor = chessColor.BLACK;
+            oppColumn = chessActivity.blackColumn;
+            oppRow = chessActivity.blackRow;
+        }
     }
 
     @Override
@@ -18,11 +36,8 @@ public class bishop extends chessPiece{
             for (int rDif = -1; rDif <= 1; rDif++) {
                 if (cDif == 0 && rDif == 0) {
                     continue;
-                } else if (cDif == rDif || cDif == -rDif || -cDif == rDif){
+                } else if (cDif == rDif || cDif == -rDif){
                     helper(cDif, rDif, legalMoves);
-                }
-                else {
-                    continue;
                 }
             }
         }
@@ -38,6 +53,7 @@ public class bishop extends chessPiece{
             if (row < 0 || row > 7) {
                 break;
             }
+            boolean pieceExist = chessBoard.pieceAt(column, row) == null;
             if (chessActivity.inCheck) {
                 boolean prevIsNull = false;
                 chessPiece prev = chessBoard.pieceAt(column, row);
@@ -46,7 +62,7 @@ public class bishop extends chessPiece{
                 }
                 boolean check = false;
                 chessActivity.boardPieces.remove(new Pair<>(this.column, this.row));
-                chessActivity.boardPieces.put(new Pair<>(column, row), new bishop(column, row, this.color));
+                chessActivity.boardPieces.put(new Pair<>(column, row), new queen(column, row, this.color));
                 for (chessPiece enemy: chessActivity.boardPieces.values()) {
                     if (enemy == null) {
                         continue;
@@ -69,6 +85,9 @@ public class bishop extends chessPiece{
                 }
             } else {
                 legalMoves.add(new Pair<>(column, row));
+                if(!pieceExist) {
+                    break;
+                }
             }
             column += columnIncre;
             row += rowIncre;
@@ -89,6 +108,15 @@ public class bishop extends chessPiece{
             chessActivity.boardPieces.put(new Pair<>(column, row), this);
             this.column = column;
             this.row = row;
+            Set<Pair<Integer, Integer>> legalMoves = getLegalMovements();
+            if (legalMoves.contains(new Pair<>(oppColumn, oppRow))) {
+                oppInCheck = true;
+            }
+            if (oppColor == chessColor.BLACK) {
+                chessActivity.blackInCheck = oppInCheck;
+            } else {
+                chessActivity.whiteInCheck = oppInCheck;
+            }
             return true;
         }
         return false;
@@ -96,6 +124,16 @@ public class bishop extends chessPiece{
 
     @Override
     boolean canCheck(int column, int row) {
-        return validateMove(column, row);
+        if (chessBoard.pieceAt(column, row) == null || chessBoard.pieceAt(column, row).color == this.color) {
+            chessPiece prev = chessBoard.pieceAt(column, row);
+            boolean prevEmpty = prev == null;
+            chessActivity.boardPieces.remove(new Pair<>(column, row));
+            Set<Pair<Integer, Integer>> legalMoves = getLegalMovements();
+            if (!prevEmpty) {
+                chessActivity.boardPieces.put(new Pair<>(column, row), prev);
+            }
+            return legalMoves.contains(new Pair<>(column, row));
+        }
+        return false;
     }
 }
--- GameSuite2/app/src/main/java/com/example/gamesuite/pawn.java
+++ GameSuite2/app/src/main/java/com/example/gamesuite/pawn.java
@@ -2,30 +2,106 @@ package com.example.gamesuite;
 
 import android.util.Pair;
 
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class pawn extends chessPiece{
+    int numMoves = 0;
+    int taking = 0; //0 moving to an empty, 1 taking piece, 2 en passant
     public pawn(int column, int row, chessColor color) {
         super(column, row, color, chessRank.PAWN, (color == chessColor.BLACK)? R.drawable.blackpawn: R.drawable.whitepawn);
     }
 
     @Override
     Set<Pair<Integer, Integer>> getLegalMovements() {
-        return null;
+        return getLegalMovements(false);
+    }
+
+    Set<Pair<Integer, Integer>> getLegalMovements(boolean king) {
+        Map<Pair<Integer, Integer>, chessPiece> chessPieces = chessActivity.boardPieces;
+        Set<Pair<Integer, Integer>> legalMovements = new HashSet<Pair<Integer, Integer>>();
+        int color = 1;
+        chessColor opposite = chessColor.BLACK;
+        if(this.color == chessColor.BLACK) {
+            color = -1;
+            opposite = chessColor.WHITE;
+        }
+        if(column < 8) {
+            if(chessPieces.containsKey(new Pair<Integer, Integer>(column+1, row + color))
+                    && (chessPieces.get(new Pair<Integer, Integer>(column + 1, row + color)).color == opposite || king)){
+                legalMovements.add(new Pair<Integer, Integer>(column + 1, row + color));
+            }
+            if(chessPieces.containsKey(new Pair<Integer, Integer>(column+1, row))) {
+                chessPiece p = chessPieces.get(new Pair<Integer, Integer>(column + 1, row));
+                if(p.color == opposite && pawn.class.isInstance(p) && ((pawn) p).numMoves == 1){
+                    legalMovements.add((new Pair<Integer, Integer>(column + 1, row + color)));
+                }
+            }
+        }
+        if(column > 0) {
+            if(chessPieces.containsKey(new Pair<Integer, Integer>(column-1, row + color))
+                    && (chessPieces.get(new Pair<Integer, Integer>(column-1, row + color)).color == opposite || king)){
+                legalMovements.add(new Pair<Integer, Integer>(column-1, row + color));
+            }
+            if(chessPieces.containsKey(new Pair<Integer, Integer>(column-1, row))) {
+                chessPiece p = chessPieces.get(new Pair<Integer, Integer>(column + 1, row));
+                if(p.color == opposite && p instanceof pawn && ((pawn) p).numMoves == 1){
+                    legalMovements.add((new Pair<Integer, Integer>(column - 1, row + color)));
+                }
+            }
+        }
+        if(!chessPieces.containsKey(new Pair<Integer, Integer>(column, row + 1)) && !king){
+            legalMovements.add(new Pair<Integer, Integer>(column, row + 1));
+        }
+        if(numMoves == 0 && !chessPieces.containsKey(new Pair<Integer, Integer>(column, row + 2)) && !king){
+            legalMovements.add(new Pair<Integer, Integer>(column, row + 2));
+        }
+        return legalMovements;
     }
 
     @Override
     boolean validateMove(int column, int row) {
-        return false;
+        Set<Pair<Integer, Integer>> legalMoves = getLegalMovements();
+        return legalMoves.contains(new Pair<Integer, Integer>(column, row));
     }
 
     @Override
     boolean move(int column, int row) {
+        int color = 1;
+        chessColor opposite = chessColor.BLACK;
+        if(this.color == chessColor.BLACK) {
+            color = -1;
+            opposite = chessColor.WHITE;
+        }
+        if(validateMove(column, row)) {
+            if(this.column == column + 1 || this.column == column - 1) {
+                if(chessActivity.boardPieces.get(new Pair<Integer, Integer>(column, row)) != null){
+                    this.taking = 1;
+                    chessActivity.boardPieces.remove(new Pair<Integer, Integer>(column, row));
+                } else {
+                    this.taking = 2;
+                    chessActivity.boardPieces.remove(new Pair<Integer, Integer>(column, row-1));
+                }
+            } else {
+                this.taking = 0;
+            }
+
+            chessActivity.boardPieces.remove(new Pair<Integer, Integer>(this.column, this.row));
+            this.column = column;
+            this.row = row;
+            chessActivity.boardPieces.put(new Pair<Integer, Integer>(this.column, this.row), this);
+            return true;
+        }
         return false;
     }
 
     @Override
     boolean canCheck(int column, int row) {
+        Set<Pair<Integer, Integer>> legalMoves = getLegalMovements(true);
+        if(legalMoves.contains(new Pair<Integer, Integer>(column, row))){
+            return true;
+        }
         return false;
     }
 }
